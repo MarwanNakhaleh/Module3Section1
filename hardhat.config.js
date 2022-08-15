7e576047c8062628edd8454c9468cff215a3a001
Marwan Nakhaleh <marwan@Marwans-MacBook-Air.local>
2022-08-15 16:51:10 -0400

    non-graded activities for Module 3 Section 1

---
+++ hardhat.config.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+
+module.exports = {
+  defaultNetwork: "matic",
+  accounts: {
+    count: 10,
+    accountBalance: 10
+  },
+  networks: {
+    hardhat: {
+    },
+    localhost: {
+    },
+    matic: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+}
